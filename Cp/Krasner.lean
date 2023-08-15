1752a45dbf89399fdb541e1cf0a296bf6be4efea
jjdishere <emailboxofjjd@163.com>
2023-08-15 23:32:00 +0800

    delete unused class

--- Cp/Krasner.lean
+++ Cp/Krasner.lean
@@ -1,10 +1,7 @@
 import Mathlib.Topology.Algebra.ValuedField
-import Mathlib.Analysis.Normed.Field.Basic
 import Mathlib.Data.Polynomial.Splits
 import Mathlib.Data.Polynomial.RingDivision
 import Mathlib.FieldTheory.Adjoin
-
-class NonarchimedeanField (K : Type _) extends NormedField K, NonarchimedeanRing K
 
 class ValuedFieldExtension (K : Type _) (L : Type _) {Γ₀ : Type _} [LinearOrderedCommGroupWithZero Γ₀] [Field K] [Field L] [vK : Valued K Γ₀] [vL : Valued L Γ₀] extends Algebra K L where
   preserve_valuation : ∀ x : K, vL.v (toRingHom x) = vK.v x  
@@ -12,4 +9,5 @@ class ValuedFieldExtension (K : Type _) (L : Type _) {Γ₀ : Type _} [LinearOrd
 #check Polynomial
 #check Polynomial.Splits
 #check Polynomial.roots
-theorem Krasner (K : Type _) [NonarchimedeanField K] {Γ₀ : Type _} [LinearOrderedCommGroupWithZero Γ₀] [Valued K Γ₀] (L : Type _) [Field L] [vL : Valued L Γ₀] [i : ValuedFieldExtension K L] (f : Polynomial K) (hirr : Irreducible f) (hsp : Polynomial.Splits i.toRingHom f) : ∀ α : L, ∀ β : L, f.eval₂ i.toRingHom (α) = 0 → (∀ α' : L, f.eval₂ i.toRingHom (α') = 0 → α' ≠ α → vL.v (α - β) < vL.v (α - α')) → (K⟮α⟯ ≤ K⟮β⟯) := sorry
+
+theorem Krasner (K : Type _) [Field K] {Γ₀ : Type _} [LinearOrderedCommGroupWithZero Γ₀] [Valued K Γ₀] (L : Type _) [Field L] [vL : Valued L Γ₀] [i : ValuedFieldExtension K L] (f : Polynomial K) (hirr : Irreducible f) (hsp : Polynomial.Splits i.toRingHom f) : ∀ α : L, ∀ β : L, f.eval₂ i.toRingHom (α) = 0 → (∀ α' : L, f.eval₂ i.toRingHom (α') = 0 → α' ≠ α → vL.v (α - β) < vL.v (α - α')) → (K⟮α⟯ ≤ K⟮β⟯) := sorry
